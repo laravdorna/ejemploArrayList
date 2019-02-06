3412e834460ca4439fad4a67ab303603ae0d6333
lvazquezdorna <lvazquezdorna@nautilus17.danielcastelao.org>
2019-02-06 11:10:01 +0100

    2.añadido metodo limpiar consola

--- src/ex/pkg11_arraylist/Ex11_ArrayList.java
+++ src/ex/pkg11_arraylist/Ex11_ArrayList.java
@@ -81,6 +81,7 @@ public class Ex11_ArrayList {
 //        }
     }
 
+    // metodo que limpia la consola para que no aparezca lo anterior 
     public void limpiarConsola() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
